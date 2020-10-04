a782c5ebfcafae0c1fe08099c51ebfc0578d5b27
MIKI <emma045621@gmail.com>
2020-10-04 18:21:39 +0800

    tcp homework

---
+++ emma/src/tcp/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	service := ":5000"
+	listener, err := net.Listen("tcp", service)
+	checkError(err)
+
+	fmt.Println("等待使用者加入")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+
+		fmt.Println("連線成功 ", conn.RemoteAddr().String())
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+
+	buf := make([]byte, 2048)
+
+	for {
+		n, err := conn.Read(buf)
+
+		if err != nil {
+			fmt.Println("收到錯誤：", err.Error())
+			return
+		}
+
+		fmt.Println("收到資料：", buf[:n])
+		conn.Write([]byte("OK"))
+	}
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("連線失敗:", err.Error())
+		os.Exit(1)
+	}
+}
