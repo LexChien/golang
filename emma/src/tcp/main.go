586bfe7c19fbc599a58e501d9329f57a6a39c6cc
MIKI <emma045621@gmail.com>
2020-10-07 14:29:07 +0800

    新增加解密

--- emma/src/tcp/main.go
+++ emma/src/tcp/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"net"
 	"os"
@@ -23,6 +24,10 @@ func main() {
 	}
 }
 
+var baseValue = []byte{
+	0x34, 0xdc, 0x86, 0xa3, 0x65, 0xda, 0x5d, 0x79, 0xc1, 0xf3, 0x9e, 0x1f, 0x45, 0x78, 0x99, 0x60, 0x89,
+}
+
 func handle(conn net.Conn) {
 
 	buf := make([]byte, 2048)
@@ -35,8 +40,31 @@ func handle(conn net.Conn) {
 			return
 		}
 
-		fmt.Println("收到資料：", buf[:n])
-		conn.Write([]byte("OK"))
+		recv_data := buf[:n]
+		for i, data := range recv_data {
+			recv_data[i] = data ^ baseValue[i]
+		}
+
+		res := 0
+		for i := 0; i < 14; i++ {
+			da := recv_data[i] ^ recv_data[i+1]
+			res += int(da)
+		}
+		p := byte(res / 256)
+		j := byte(res % 256)
+
+		retrunData := "NG"
+		if p == recv_data[15] && j == recv_data[16] {
+			retrunData = "OK"
+		} else {
+			fmt.Println("比較錯誤")
+		}
+		fmt.Println("收到資料：", buf[:n], "p=", hex.EncodeToString([]byte{p}), "j=", hex.EncodeToString([]byte{j}), retrunData)
+		_, werr := conn.Write([]byte(retrunData))
+		if werr != nil {
+			fmt.Println(werr)
+			break
+		}
 	}
 
 }
