c42b9ff9274c35330acc82bbdae3111deccbb898
tina813305 <ix813305@gmail.com>
2020-10-11 21:20:15 +0800

    api video test

--- ix813305/video/main.go
+++ ix813305/video/main.go
@@ -10,9 +10,20 @@ func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 	router.Static("/js", "js")
-	router.GET("/web", getIndex)
+	// router.GET("/web", getIndex)
+	router.GET("/video", getVideo)
+	router.GET("/videoIos", getVideoIos)
 	router.Run(":3388")
 }
-func getIndex(ctx *gin.Context) {
-	ctx.HTML(http.StatusOK, "index.html", nil)
+
+// func getIndex(ctx *gin.Context) {
+// 	ctx.HTML(http.StatusOK, "index.html", nil)
+// }
+func getVideo(ctx *gin.Context) {
+	//flv測試
+	ctx.HTML(http.StatusOK, "video.html", nil)
+}
+func getVideoIos(ctx *gin.Context) {
+	//ios測試
+	ctx.HTML(http.StatusOK, "video2.html", nil)
 }
